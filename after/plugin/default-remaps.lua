0e0bb058d8ca826482cd12798c76b8dad8cd574d
jmadotgg <julius.makel@gmail.com>
2024-05-10 16:11:31 +0200

    Add oil file browser

--- after/plugin/default-remaps.lua
+++ after/plugin/default-remaps.lua
@@ -8,6 +8,12 @@
 -- s -> deletes character under cursor and puts me into insert mode
 -- *:%s//new_word -> hover over word and replace all occurences with new string without typing variable again
 -- cga -> deletes current word and goes into insert mode, rename word and then press . in normal mode to automatically apply to next wor, skip word with n
+-- :e %:h open directory view in current file directory
+-- :e %:h/filename create filename file in current directory
+-- :e. open directory view
+-- gt and gT cicle through neovim tabs
+-- number-gt jump to tab position
+-- :cd dir -> set working directory so that files are found
 
 ---------------
 -- Line numbers
