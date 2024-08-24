a48f502e19d0b19b1c9c947031c432935e5c4129
jmadotgg <julius.makel@gmail.com>
2024-08-24 14:13:16 +0200

    Add type hint in autocompletion (mainly for tailwind color preview)

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -1,27 +1,33 @@
 -- Setup language servers.
-local lspconfig = require('lspconfig')
-lspconfig.wgsl_analyzer.setup {}
-lspconfig.pyright.setup {}
+local lspconfig = require("lspconfig")
+lspconfig.wgsl_analyzer.setup({})
+lspconfig.pyright.setup({})
 
 -- html
 local html_capabilities = vim.lsp.protocol.make_client_capabilities()
 html_capabilities.textDocument.completion.completionItem.snippetSupport = true
-lspconfig.html.setup {
+lspconfig.html.setup({
 	capabilities = html_capabilities,
-	filetypes = { "html", "templ", "htmldjango" }
-}
+	filetypes = { "html", "templ", "htmldjango" },
+})
 -- css
-lspconfig.cssls.setup {
-	capabilities = html_capabilities
-}
+lspconfig.cssls.setup({
+	capabilities = html_capabilities,
+})
 
 -- tailwindcss
-lspconfig.tailwindcss.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.htmx.setup {
-	filetypes = { "html", "templ", "htmldjango" }
-}
-lspconfig.lua_ls.setup {
+lspconfig.tailwindcss.setup({})
+
+-- ts
+lspconfig.tsserver.setup({})
+
+-- htmx
+lspconfig.htmx.setup({
+	filetypes = { "html", "templ", "htmldjango" },
+})
+
+-- lua
+lspconfig.lua_ls.setup({
 	settings = {
 		Lua = {
 			workspace = { checkThirdParty = false, library = vim.api.nvim_get_runtime_file("", true) },
@@ -31,51 +37,52 @@ lspconfig.lua_ls.setup {
 			},
 		},
 	},
-}
-lspconfig.rust_analyzer.setup {
+})
+
+-- rust
+lspconfig.rust_analyzer.setup({
 	-- Server-specific settings. See `:help lspconfig-setup`
 	settings = {
-		['rust-analyzer'] = {},
+		["rust-analyzer"] = {},
 	},
-}
-
+})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 --
 local conform_formatter = require("conform")
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
 		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 		-- Buffer local mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wl', function()
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>f', function()
-			conform_formatter.format({ async = true})
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			conform_formatter.format({ async = true })
 			--vim.lsp.buf.format { async = true }
 		end, opts)
 	end,
