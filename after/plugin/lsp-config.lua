75d98193bfc16f6c3f9a4665cb0defcde81a9f2c
juliusbgw <julius.makel@bgw-online.de>
2025-09-08 15:22:24 +0200

    remove mason-null-ls

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -10,7 +10,7 @@ capabilitites.textDocument.completion.completionItem.resolveSupport = {
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.cssls.setup {
 	capabilitites = capabilitites
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,5 +2,4 @@ return {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
-	"mason-null-ls"
 }
