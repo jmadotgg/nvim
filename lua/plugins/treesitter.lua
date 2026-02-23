32d4f83d3c9dab2ce2f28f014635adfc8073c48b
Julius Makel <julius.makel@gmail.com>
2026-02-23 21:39:07 +0100

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.install").compilers = { "clang" }
 
-		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "wgsl", "css" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+    local parsers_to_install = {
+      "c", "lua", "vim", "vimdoc", "query", "elixir", "heex",
+      "javascript", "html", "rust", "css", "wgsl"
+    }
+    require("nvim-treesitter.install").install(parsers_to_install)
+
+    vim.treesitter.configs = vim.treesitter.configs or {}
+    vim.treesitter.configs.highlight = { enable = true }
+    vim.treesitter.configs.indent = { enable = true }
+
+    -- REMOVED: ts.start() autocmd - Treesitter auto-attaches now!
+  end,
 }
+
